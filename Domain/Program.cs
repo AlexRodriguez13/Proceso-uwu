f4a2836810ff9c4fb0800cea030c0bc6b62b439b
AlexRodriguez13 <bryanalexandercb13@gmail.com>
2022-04-19 10:10:18 -0600

    changes in program (main)

--- Domain/Program.cs
+++ Domain/Program.cs
@@ -18,7 +18,7 @@ namespace Domain
             //Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Form1());
             
-                Application.Run(new FibonacciForm());
+            Application.Run(new FibonacciForm());
             
         }
     }
